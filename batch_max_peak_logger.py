8c9347668f8812a9f62b325253d00f3db95e242b
twillecke <thiago.willecke@ino-games.com>
2024-05-09 11:40:03 -0300

    chore: remove comments; fix typos

--- batch_max_peak_logger.py
+++ batch_max_peak_logger.py
@@ -19,12 +19,9 @@ def process_files(directory):
                 if filename.lower().endswith((".wav", ".mp3", ".ogg", ".flac")):
                     source_rms = rms(file_address)
                     if source_rms is not None:
-                        # print("{:<50} {:<30} Source Max Peak: {:.2f} dBFS".format(file_address, filename, source_rms))
-                        check_str = "  ✓" if source_rms < -3 else "  ✕  (FAILED: Peak should be below -3dbFS)"
+                        check_str = "  ✓" if source_rms < -3 else "  ✕  (FAILED: Peak should be strictly below -3dbFS)"
                         print("{:<70} Source Max Peak: {:.2f} dBFS".format(file_address, source_rms) + check_str)
 
     except Exception as e:
         print(f"Error: {e}")
 
-# directory_path = "./BASE"
-# process_files(directory_path)
--- batch_rms_logger.py
+++ batch_rms_logger.py
@@ -19,12 +19,7 @@ def process_files(directory):
                 if filename.lower().endswith((".wav", ".mp3", ".ogg", ".flac")):
                     source_rms = rms(file_address)
                     if source_rms is not None:
-                        # print("{:<50} {:<30} Source RMS: {:.2f} dBFS".format(file_address, filename, source_rms))
-                        check_str = "  ✓" if -20 <= source_rms <= -12 else "  ✕  (FAILED: RMS should be between -12 and -20)"
+                        check_str = "  ✓" if -20 <= source_rms <= -12 else "  ✕  (FAILED: RMS should be in between -12dbFS and -20dbFS)"
                         print("{:<70}  Source RMS: {:.2f} dBFS".format(file_address,source_rms) + check_str)                        
     except Exception as e:
         print(f"Error: {e}")
-
-
-# directory_path = "./BASE"
-# process_files(directory_path)
